1ca6aac4892a36402438e6058aa70fd5ee913865
Clément Remy <clement.remy@ens-lyon.fr>
2026-03-06 12:48:49 +0100

    Allocator overlapping test

--- tests/allocator_nooverlap.cpp
+++ tests/allocator_nooverlap.cpp
@@ -1,13 +1,29 @@
 
 
+
 #include <iterator>
 #include <random>
+#include <set>
+#include <spirv/unified1/spirv.h>
+#include <vulkan/vulkan_core.h>
 
 #include "../vroum3d/vroum3d.h"
 
 using namespace Vroum3d;
 
 constexpr std::size_t n_blocks = 10000;
+
+struct Segment
+{
+  VkDeviceMemory mem;
+  VkDeviceSize size;
+  VkDeviceSize offset;
+
+  bool operator<(const Segment& rhs) const
+  {
+    return mem < rhs.mem || (mem == rhs.mem && offset < rhs.offset);
+  }
+};
 
 int VROUM3D_MAIN()
 {
@@ -20,6 +36,7 @@ int VROUM3D_MAIN()
   Instance inst(disp);
 
   std::vector<Allocator::allocated_memory_t> blocks;
+  std::set<Segment> segms;
 
 	for(std::size_t i = 0; i != n_blocks;++i)
 	{
@@ -31,11 +48,27 @@ int VROUM3D_MAIN()
 			if(bd(rng))
 				size |= 1ull << i;
 
-    auto new_block = inst.allocate(0, size);
+    auto block = inst.allocate(0, size);
+    blocks.push_back(block);
 
-    check(new_block.size() >= size);
+    Segment new_seg{block.memory(), block.size(), block.offset()};
+    // Check for overlap in blocks
+    
+    
+    if(auto ub = segms.upper_bound(new_seg); ub != segms.begin())
+    {
+      ub = std::prev(ub);
+      if(ub->mem == new_seg.mem)
+        check(ub->size + ub->offset <= new_seg.offset);
+    }
+    
+    if(auto lb = segms.lower_bound(new_seg); lb != segms.end())
+    {
+      if(lb->mem == new_seg.mem)
+        check(lb->offset >= new_seg.offset + new_seg.size);
+    }
 
-    blocks.push_back(new_block);
+    segms.insert(new_seg);
 	}
 
   while(!blocks.empty())
